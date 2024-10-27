b85cb2261b1a5ff76c5151bed845d70b5c29adc0
kyle <mcphylus@live.co.uk>
2024-10-27 15:39:42 +0000

    documentation

---
+++ PluginDemo/StartsWithTestHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+using TestManager.PluginLib;
+
+namespace PluginDemo;
+public class StartsWithTestHandler : ITestHandler<StartWithConfig>
+{
+    public string Type => "StartsWith";
+
+    public Task RunTest(ITestParameters<StartWithConfig> testParameters, TestResult testResult)
+    {
+        var fileText = Encoding.UTF8.GetString(testParameters.File);
+
+        testResult.AddResult("main", fileText.StartsWith(testParameters.Parameters.Text), message: "custom message i could populate with useful info in the even of failure");
+
+        return Task.CompletedTask;
+    }
+}
+
+public class StartWithConfig
+{
+    public string Text { get; set; }
+}
